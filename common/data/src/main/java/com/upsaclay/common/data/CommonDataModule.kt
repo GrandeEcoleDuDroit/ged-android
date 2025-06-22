c38428ca569e97f0fad19dbafe3f9f8594dc30a2
Mourchidi <mmfoumby@unowhy.com>
2025-06-22 16:40:11 +0200

    Update CommonDataModule.kt

--- common/data/src/main/java/com/upsaclay/common/data/CommonDataModule.kt
+++ common/data/src/main/java/com/upsaclay/common/data/CommonDataModule.kt
@@ -57,7 +57,7 @@ val commonDataModule = module {
 
     single<Retrofit>(GED_SERVER_QUALIFIER) {
         Retrofit.Builder()
-            .baseUrl("http://192.168.1.67:3000")
+            .baseUrl(BuildConfig.SERVER_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
