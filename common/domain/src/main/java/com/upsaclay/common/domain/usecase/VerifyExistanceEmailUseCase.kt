d062525dcf133e5c3fd1e56bbfddd45c2fb3230a
zykogithub <modaherry@gmail.com>
2025-06-27 01:05:50 +0200

    suppression de fichier inutile

--- common/domain/src/main/java/com/upsaclay/common/domain/usecase/VerifyExistanceEmailUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.upsaclay.common.domain.usecase
-
-class VerifyExistanceEmailUseCase {
-
-    operator fun invoke(email : String) : Boolean {
-        TODO("vérifie l'existance de l'email" +
-                "renvoyer erreur du type forbidenException")
-    }
-}
