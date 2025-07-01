68dedb68006798c8c0b10a45d1d392ef71692e3e
Mourchidi <mmfoumby@unowhy.com>
2025-07-01 22:00:34 +0200

    Update target sdk from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.upsaclay.gedoise"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.upsaclay.gedoise"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 3
         versionName = "1.0"
 
