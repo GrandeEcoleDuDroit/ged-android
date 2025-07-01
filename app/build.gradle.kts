7066d15411025e81f228c84127bf7eff0441d9b0
Mourchidi <mmfoumby@unowhy.com>
2025-07-01 22:03:25 +0200

    update version code to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.upsaclay.gedoise"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
