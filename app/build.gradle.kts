d477a938727156e08b94dc3f7b1ff22dcb72e0a4
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2025-09-20 12:11:58 +0200

    Update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.upsaclay.gedoise"
         minSdk = 29
         targetSdk = 35
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
