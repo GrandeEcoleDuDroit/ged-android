94ccf303c3a5dbd45682c5c107c3d1ce1649766e
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2025-10-18 17:37:21 +0200

    Increment version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.upsaclay.gedoise"
         minSdk = 29
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
