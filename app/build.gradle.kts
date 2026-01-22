517c1b6a3e0767a4b9b5da9c00f937992ef5d019
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2026-01-22 17:10:54 +0100

    Increase version code to 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "com.upsaclay.gedoise"
         minSdk = 29
         targetSdk = 35
-        versionCode = 11
+        versionCode = 12
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
