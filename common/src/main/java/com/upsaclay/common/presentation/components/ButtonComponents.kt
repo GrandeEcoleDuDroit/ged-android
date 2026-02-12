b7ecc0ccab42db0795cf52b00372f59f214d0c73
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2026-02-12 23:32:31 +0100

    feat:AGED-325: Add "plus" Icon button in TitleTopBar, add "plus" Icon button and DropDownMenu in NewsTopBar, remove floating action buttons

--- common/src/main/java/com/upsaclay/common/presentation/components/ButtonComponents.kt
+++ common/src/main/java/com/upsaclay/common/presentation/components/ButtonComponents.kt
@@ -1,54 +1,21 @@
 package com.upsaclay.common.presentation.components
 
-import android.content.res.Configuration
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandHorizontally
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkOut
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.upsaclay.common.R
-import com.upsaclay.common.extension.extraSmallSpacing
-import com.upsaclay.common.extension.smallSpacing
 import com.upsaclay.common.presentation.theme.GedoiseTheme
-import com.upsaclay.common.presentation.theme.closeFABContainerColor
-import com.upsaclay.common.presentation.theme.closeFABContentColor
 import com.upsaclay.common.presentation.theme.loadingButtonColors
 import com.upsaclay.common.presentation.theme.white
 
@@ -106,20 +73,6 @@ fun BackButton(
 }
 
 @Composable
-fun SimpleFloatingActionButton(
-    modifier: Modifier = Modifier,
-    icon: @Composable () -> Unit,
-    onClick: () -> Unit
-) {
-    FloatingActionButton (
-        onClick = onClick,
-        modifier = modifier,
-        containerColor = MaterialTheme.colorScheme.secondaryContainer,
-        content = icon
-    )
-}
-
-@Composable
 fun LoadingButton(
     modifier: Modifier = Modifier,
     text: String,
@@ -141,79 +94,6 @@ fun LoadingButton(
             )
         } else {
             Text(text = text)
-        }
-    }
-}
-
-@Composable
-fun FloatingActionButtonMenu(
-    modifier: Modifier = Modifier,
-    expanded: Boolean,
-    buttonIcon: @Composable () -> Unit,
-    onButtonClick: () -> Unit,
-    onCloseClick: () -> Unit,
-    content: @Composable ColumnScope.() -> Unit
-) {
-    val radius = if (expanded) dimensionResource(R.dimen.fab_size) / 2f else dimensionResource(R.dimen.fab_corner_size)
-    val cornerRadius by animateDpAsState(targetValue = radius)
-    val containerColor = if(expanded) MaterialTheme.colorScheme.closeFABContainerColor else FloatingActionButtonDefaults.containerColor
-    val contentColor = if(expanded) MaterialTheme.colorScheme.closeFABContentColor else contentColorFor(FloatingActionButtonDefaults.containerColor)
-
-    Column(
-        modifier = modifier.clip(FloatingActionButtonDefaults.shape),
-        horizontalAlignment = Alignment.End,
-        verticalArrangement = Arrangement.smallSpacing()
-    ) {
-        AnimatedVisibility(
-            visible = expanded,
-            enter = fadeIn() + expandHorizontally(),
-            exit = fadeOut(animationSpec = tween(durationMillis = 100)) + shrinkOut()
-        ) {
-            Column(
-                verticalArrangement = Arrangement.extraSmallSpacing(),
-                horizontalAlignment = Alignment.End
-            ) {
-                content()
-            }
-        }
-
-        FloatingActionButton(
-            shape = RoundedCornerShape(cornerRadius),
-            containerColor = containerColor,
-            contentColor = contentColor,
-            onClick = if (expanded) onCloseClick else onButtonClick
-        ) {
-            if (expanded) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_close),
-                    contentDescription = null
-                )
-            } else {
-                buttonIcon()
-            }
-        }
-    }
-}
-
-
-@Composable
-fun FabMenuItem(
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit,
-    icon: @Composable () -> Unit,
-    text: @Composable () -> Unit
-) {
-    ExtendedFloatingActionButton(
-        modifier = modifier,
-        shape = CircleShape,
-        onClick = onClick
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.smallSpacing()
-        ) {
-            icon()
-            text()
         }
     }
 }
@@ -254,48 +134,5 @@ private fun LoadingButtonPreview() {
             onClick = {},
             loading = false
         )
-    }
-}
-
-@Preview
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-private fun FloatingActionButtonMenuPreview() {
-    var expanded by remember { mutableStateOf(true) }
-
-    GedoiseTheme {
-        FloatingActionButtonMenu(
-            expanded = expanded,
-            buttonIcon = {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = null
-                )
-            },
-            onButtonClick = { expanded = true },
-            onCloseClick = { expanded = false }
-        ) {
-            FabMenuItem(
-                onClick = {},
-                icon = {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = null
-                    )
-                },
-                text = { Text(text = "Add") }
-            )
-
-            FabMenuItem(
-                onClick = {},
-                icon = {
-                    Icon(
-                        imageVector = Icons.Default.MailOutline,
-                        contentDescription = null
-                    )
-                },
-                text = { Text(text = "Email") }
-            )
-        }
     }
 }
--- news/src/main/java/com/upsaclay/news/presentation/news/components/NewsScaffold.kt
+++ news/src/main/java/com/upsaclay/news/presentation/news/components/NewsScaffold.kt
@@ -2,7 +2,6 @@ package com.upsaclay.news.presentation.news.components
 
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
@@ -16,16 +15,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.upsaclay.common.domain.entity.User
 import com.upsaclay.common.domain.userFixture
-import com.upsaclay.common.presentation.components.FabMenuItem
-import com.upsaclay.common.presentation.components.FloatingActionButtonMenu
 import com.upsaclay.common.presentation.theme.GedoiseTheme
 import com.upsaclay.common.utils.PhonePreviews
-import com.upsaclay.news.R
 
 @Composable
 fun NewsScaffold(
@@ -43,48 +37,20 @@ fun NewsScaffold(
             .pointerInput(Unit) {
                 detectTapGestures(onTap = { expanded = false })
             },
-        topBar = { NewsTopBar() },
+        topBar = {
+            NewsTopBar(
+                admin = user.admin,
+                expanded = expanded,
+                onOpenMenuButtonClick = { expanded = true },
+                onDismissMenu = { expanded = false },
+                onCreateAnnouncementClick = onCreateAnnouncementClick,
+                onCreatePostClick = onCreatePostClick
+            )
+        },
         bottomBar = bottomBar,
         snackbarHost = {
             SnackbarHost(snackbarHostState) {
                 Snackbar(it)
-            }
-        },
-        floatingActionButton = {
-            if (user.admin) {
-                FloatingActionButtonMenu(
-                    expanded = expanded,
-                    buttonIcon = {
-                        Icon(
-                            painter = painterResource(com.upsaclay.common.R.drawable.ic_add),
-                            contentDescription = stringResource(id = R.string.new_announcement)
-                        )
-                    },
-                    onButtonClick = { expanded = true },
-                    onCloseClick = { expanded = false }
-                ) {
-                    FabMenuItem(
-                        onClick = onCreatePostClick,
-                        icon = {
-                            Icon(
-                                painter = painterResource(R.drawable.ic_outline_post),
-                                contentDescription = stringResource(id = R.string.new_post)
-                            )
-                        },
-                        text = { Text(text = stringResource(id = R.string.new_post)) }
-                    )
-
-                    FabMenuItem(
-                        onClick = onCreateAnnouncementClick,
-                        icon = {
-                            Icon(
-                                painter = painterResource(R.drawable.ic_outline_campaign),
-                                contentDescription = stringResource(id = R.string.new_announcement)
-                            )
-                        },
-                        text = { Text(text = stringResource(id = R.string.new_announcement)) }
-                    )
-                }
             }
         },
         content = content
@@ -115,7 +81,7 @@ private fun NewsScaffoldPreview() {
 @Preview
 @Composable
 fun SmartExpandableText() {
-    GedoiseTheme() {
+    GedoiseTheme {
         OutlinedButton({}) {
             Text("YES")
         }
